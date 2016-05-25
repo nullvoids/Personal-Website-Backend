7a232ab2b71973d8759490caf059d10debcbceef
sollabeck <kevin.ceballos@gmail.com>
2016-05-24 20:58:39 -0700

    Change attributes to camelcase instead of headache-inducing kebab-case

--- config/initializers/ams_json_adapter.rb
+++ config/initializers/ams_json_adapter.rb
@@ -1,1 +1,12 @@
+api_mime_types = %W(
+application/vnd.api+json
+text/x-json
+application/json
+)
+
+Mime::Type.unregister :json
+Mime::Type.register 'application/json', :json, api_mime_types
 ActiveModel::Serializer.config.adapter = ActiveModelSerializers::Adapter::JsonApi
+ActiveModelSerializers.config.key_transform = :camel_lower
+
+
